4597b54619779f9db72799471b22835a3e64355f
BOBILLEChristophe <38000184+BOBILLEChristophe@users.noreply.github.com>
2023-10-04 23:37:57 +0200

    Update DCCxx-ESP32-Railcom.ino

--- src/DCCxx-ESP32-Railcom.ino
+++ src/DCCxx-ESP32-Railcom.ino
@@ -25,7 +25,7 @@
   - 100 -> la vitesse en pas (sur un maxi de 128) Pour des raisons de simplification, seul le protocole à 128 crans est implanté
   - 1 -> la direction (1 = sens avant, 0 = sens arrière)
 
-  Une commande de traction s'écrit de la manière suivante : <f 31 144>
+  Une commande de fonction s'écrit de la manière suivante : <f 31 144>
   //      128 à 159 => F0_F4
   //      176 à 191 => F5_F8
   //      160 à 175 => F9_F12
@@ -38,9 +38,10 @@
   Cette station n'a pour l'instant été testée qu'avec la carte moteur LMD18200 et adopte le brochage de DCC++ pour la voie principale (main)
   Par défaut dans ce programme, le brochage est :
 
-  #define PIN_PWM       GPIO_NUM_12   // ENABLE (PWM)
-  #define PIN_DIR       GPIO_NUM_13   // SIGNAL (DIR)
-  #define PIN_BRAKE     GPIO_NUM_14   // CUTOUT (BRAKE)
+  #define PIN_PWM                   GPIO_NUM_12   // ENABLE (PWM)
+  #define PIN_DIR                   GPIO_NUM_13   // SIGNAL (DIR)
+  #define PIN_BRAKE                 GPIO_NUM_14   // CUTOUT (BRAKE)
+  #define CURRENT_MONITOR_PIN_MAIN  GPIO_NUM_35   // Mesure de courant
 
 ****************************************************************************************************************************************
   ATTENTION à parametrer le protection de courant à une valeur correspondant à votre configuration
@@ -51,8 +52,10 @@
         Original description from DCC++ BASE STATION a C++ program written by Gregg E. Berman GNU General Public License.
         Original description from Pascal Barlier GNU General Public License
 
-        v 1.5 oct 2022
-
+        v 1.6 oct  2022
+        v 1.7 janv 2023  : Inclus la réception de commandes de traction par le bus CAN
+        v 1.8 - 15 mars 2023  : Ajout de eStop, fonction emergency()
+        v 1.9 - 23 mars 2023  : Optimisation du code
 */
 
 #ifndef ARDUINO_ARCH_ESP32
@@ -63,12 +66,13 @@
 #include "Config.h"
 #include "CurrentMonitor.h"
 
+#ifdef CAN_INTERFACE
+#include <ACAN_ESP32.h>
+#endif
+
 // Mesure de courant
 char msg[] = "<p2>";
 CurrentMonitor mainMonitor(CURRENT_MONITOR_PIN_MAIN, msg); // create monitor for current on Main Track
-
-void Task0(void *pvParameters);
-const uint32_t stackSize = 20000;
 
 #if COMM_INTERFACE == 0
 HardwareSerial *CLIENT = &Serial;
@@ -78,6 +82,10 @@ WiFiClient *CLIENT = nullptr;
 #endif
 
 DCC dcc;
+
+void Task0(void *pvParameters);
+void Task1(void *pvParameters);
+void Task2(void *pvParameters);
 
 void comm(INTERFACE *);
 
@@ -90,6 +98,24 @@ void setup()
   Serial.printf("\nVersion   :      %s\n", VERSION);
   Serial.printf("\nFichier   :      %s\n", __FILE__);
   Serial.printf("\nCompiled  :      %s - %s \n\n", __DATE__, __TIME__);
+
+#ifdef CAN_INTERFACE
+  //--- Configure ESP32 CAN
+  Serial.println("Configure ESP32 CAN");
+  ACAN_ESP32_Settings settings(CAN_BITRATE);
+  settings.mRxPin = CAN_RX;
+  settings.mTxPin = CAN_TX;
+  const ACAN_ESP32_Filter filter = ACAN_ESP32_Filter::singleStandardFilter(ACAN_ESP32_Filter::data, 0x1FA, 0);
+  const uint32_t errorCode = ACAN_ESP32::can.begin(settings, filter);
+  if (errorCode == 0)
+    Serial.println("Can configuration OK !\n");
+  else
+  {
+    Serial.printf("Can configuration error 0x%x\n", errorCode);
+    delay(1000);
+    return;
+  }
+#endif
 
   // Infos ESP32
   // esp_chip_info_t out_info;
@@ -130,73 +156,120 @@ void setup()
   dcc.clear();
   mainMonitor.setup(CLIENT);
 
-  xTaskCreate(
-    Task0,     /* Task function. */
-    "Task0",   /* String with name of task. */
-    stackSize, /* Stack size in words. */
-    NULL,      /* Parameter passed as input of the task */
-    1,         /* Priority of the task. */
-    NULL);     /* Task handle. */
+  xTaskCreatePinnedToCore(
+      Task0,    /* Task function. */
+      "Task0",  /* String with name of task. */
+      3 * 1024, /* Stack size. */
+      NULL,     /* Parameter passed as input of the task */
+      2,        /* Priority of the task. */
+      NULL,     /* Task handle. */
+      0);       /* Core. */
+
+  xTaskCreatePinnedToCore(
+      Task1,
+      "Task1",
+      2 * 1024,
+      NULL,
+      2,
+      NULL,
+      1); /* Core. */
+
+  xTaskCreatePinnedToCore(
+      Task2,
+      "Task2",
+      2 * 1024,
+      NULL,
+      1,
+      NULL,
+      1); /* Core. */
 
   Serial.printf("End setup\n\n");
 }
 
-void Task0(void *parameter)
+void Task0(void *p)
 {
+
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+
   for (;;)
   {
     mainMonitor.check();
-    delay(1);
+    vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(1));
   }
-  vTaskDelete(NULL);
 }
 
-char c;
-char commandString[16];
-
-void loop()
+void Task1(void *p)
 {
-#if COMM_INTERFACE == 0
-  CLIENT = &Serial;
-  comm(CLIENT);
-#elif COMM_INTERFACE == 1
-  WiFiClient client = SERVER.available();
-  if (client)
-  {
-    CLIENT = &client;
-    while (CLIENT->connected())
-      comm(CLIENT);
-  }
-#endif
-  delay(1);
-}
+  TickType_t xLastWakeTime = xTaskGetTickCount();
 
-void comm(INTERFACE *client)
-{
-  if (CLIENT->available())
+  auto comm = [](INTERFACE *CLIENT)
   {
-    c = CLIENT->read();
-    switch (c)
+    if (CLIENT->available())
     {
-      case '<':
-        strcpy(commandString, "\0");
-        break;
-      case '>':
-        //Serial.println(commandString);
-        dcc.parse(commandString, CLIENT);
-        break;
-      default:
-        sprintf(commandString, "%s%c", commandString, c);
+      char c;
+      char commandTxt[16];
+      size_t commandLen = 0;
+      for (uint8_t i = 0; i < 16; i++)
+      {
+        c = CLIENT->read();
+        switch (c)
+        {
+        case '<':
+          commandLen = 0;
+          break;
+        case '>':
+          commandTxt[commandLen] = '\0';
+          dcc.parse(commandTxt, CLIENT);
+          break;
+        default:
+          commandTxt[commandLen++] = c;
+        }
+      }
     }
-  }
-  mainMonitor.over(CLIENT);
-#if PRINT_CURRENT
-  static uint16_t compt = 0;
-  if (compt == 1000ul)
+  };
+
+  for (;;)
   {
-    CLIENT->printf("<a %d>", (int)(mainMonitor.current() / 4));
-    compt = 0;
-  }
-  compt++;
+#if COMM_INTERFACE == 0
+    CLIENT = &Serial;
+    comm(CLIENT);
+#elif COMM_INTERFACE == 1
+    WiFiClient client = SERVER.available();
+    if (client)
+    {
+      CLIENT = &client;
+      while (CLIENT->connected())
+        comm(CLIENT);
+    }
 #endif
+#ifdef CAN_INTERFACE
+    CANMessage frame;
+    if (ACAN_ESP32::can.receive(frame))
+    {
+      Serial.println(frame.data[0]);
+      if (frame.data[0] == 0xF0)
+      {
+        //      uint16_t locoAddr = frame.data[1] << 8 + frame.data[2];
+        //      uint8_t locoSpeed = frame.data[3];
+        //      uint8_t locoDir = frame.data[4];
+        dcc.setThrottle((frame.data[1] << 8) + frame.data[2], frame.data[3], frame.data[4]);
+      }
+    }
+#endif
+    vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(1));
+  }
 }
+
+void Task2(void *p)
+{
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+
+  for (;;)
+  {
+    mainMonitor.over(CLIENT);
+    CLIENT->printf("<a %d>", (int)(mainMonitor.current() / 4));
+    vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(1000));
+  }
+}
+
+void loop() {}
